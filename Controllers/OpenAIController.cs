6cbf1f9ae6081fab404135c8558a1f22c8795e46
hungpt <hungpt@ltsgroup.tech>
2024-11-28 13:10:30 +0700

    first api

---
+++ Controllers/OpenAIController.cs
@@ -0,0 +1,47 @@
+﻿using LanguageTranningSystem.API.Request;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LanguageTranningSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _openAiKey;
+        public OpenAIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _openAiKey = configuration["OpenAiKey"] ?? string.Empty;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TextToSpeech([FromBody] TTSRequest input)
+        {
+            var httpClient = new HttpClient();
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://api.openai.com/v1/audio/speech"),
+                Headers =
+                {
+                    { HttpRequestHeader.Authorization.ToString(), $"Bearer {_openAiKey}" },
+                    { HttpRequestHeader.Accept.ToString(), "application/json" }
+                },
+                Content = JsonContent.Create(input)
+            };
+
+            var response = await httpClient.SendAsync(httpRequestMessage);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                return BadRequest(new { Error = errorContent });
+            }
+
+            var byteArray = await response.Content.ReadAsByteArrayAsync();
+            await System.IO.File.WriteAllBytesAsync("audio.mp3", byteArray);
+            return Ok("Audio file saved successfully");
+        }
+    }
+}
---
+++ Request/TTSRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageTranningSystem.API.Request
+{
+    public record TTSRequest
+    {
+        public string Model { get; init; } = "tts-1-hd";
+        public string Input { get; init; }
+        public string Voice { get; init; } = "shimmer";
+        public string? Response_format { get; init; } = "mp3";
+        public double speed { get; set; } = 1;
+    }
+}
